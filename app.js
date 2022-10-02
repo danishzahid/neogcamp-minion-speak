d736bf7f5c69685a1475f0d0577754a15d5efd31
Danish Zahid <danishst.johns@gmail.com>
2022-10-02 12:07:38 +0530

    basic processing done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked");
+    outputDiv.innerText = "ajkklhjsgch"+txtInput.value;
 })
