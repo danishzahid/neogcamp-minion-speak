16b75a304e1c94f6c8218aaf83f9c19b4280e12c
Danish Zahid <danishst.johns@gmail.com>
2022-10-02 11:43:51 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked");
+})
