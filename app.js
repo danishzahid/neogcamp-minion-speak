b37084cd159d79e7b384f6603a961d3d573bec0b
Danish Zahid <danishst.johns@gmail.com>
2022-10-02 13:58:53 +0530

    Processing logic done

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    outputDiv.innerText = "ajkklhjsgch"+txtInput.value;
-})
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(param){
+    return serverURL+"?"+"text="+param
+}
+
+function errorHandler(error){
+    alert("something went wrong! Try again after sometime")
+}
+
+function clickEventHandler(){
+    var inputText = txtInput.value;//taking input for processing
+
+    fetch(getTranslationURL(inputText))
+    .then(Response => Response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",  clickEventHandler)
